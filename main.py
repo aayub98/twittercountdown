714050271fd37283e3ad28949c84f72db0f6cc12
Ahmad Ayub <aayub98@vt.edu>
2020-02-28 23:17:17 -0600

    trying to get ready for constant running

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ from twitter import *
 from datetime import datetime
 import sys
 sys.path.append(".")
-new_status = "testing testing"
+
+#the part where you figure out how many days are left
+def calculate_dates():
+    now = datetime.now()
+    gradDay = datetime(2020, 2, 27)
+    return (gradDay - now.today()).days + 1
+
+new_status = 'happy ' + str(calculate_dates()) + ' days until grad'
 
 with open("config.yml", 'r') as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
-twitter = Twitter(auth = OAuth(cfg['twitterkeys']['access_token_key'],
-                  cfg['twitterkeys']['access_token_secret'],
-                  cfg['twitterkeys']['consumer_key'],
-                  cfg['twitterkeys']['consumer_secret']))
+while True:
+    if (datetime.now().hour is 23 and datetime.now().minute is 15 and datetime.now().second is 0):
+        # twitter = Twitter(auth = OAuth(cfg['twitterkeys']['access_token_key'],
+        #           cfg['twitterkeys']['access_token_secret'],
+        #           cfg['twitterkeys']['consumer_key'],
+        #           cfg['twitterkeys']['consumer_secret']))
+        # results = twitter.statuses.update(status = new_status)
+        print('hello')
+# print("updated status: %s" % new_status)
 
-
-results = twitter.statuses.update(status = new_status)
-print("updated status: %s" % new_status)
